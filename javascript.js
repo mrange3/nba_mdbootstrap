5fe74500d251e908b02e8de06b5a30b1b40ec292
mrange3 <matthew.range91@gmail.com>
2019-02-28 19:48:24 -0500

    home page

---
+++ javascript.js
@@ -0,0 +1,238 @@
+
+//==================== Todays Date =========================
+
+var today = new Date();
+
+function schedule(today) {
+
+//====================== Get Request for Todays Games ============
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+var day  = today.getDay();
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", 
+"Thursday", "Friday", "Saturday"];
+ 
+todaySchedule = dayNames[day] + ", " + monthNames[mm - 1] + " " + dd + ", " + yyyy
+
+$("#date").text(todaySchedule);      // Append the new elements 
+
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+
+if (mm < 10) {
+  mm = '0' + mm;
+}
+
+
+today = yyyy + mm + dd;
+today = parseInt(today) 
+
+
+
+var gameUrl = "https://api.mysportsfeeds.com/v2.0/pull/nba/current/date/" + today + "/games.json"
+var teamsUrl = "https://api.mysportsfeeds.com/v2.0/pull/nba/current/team_stats_totals.json"
+
+
+var api = config.MY_KEY
+
+
+
+
+$.ajax
+  ({
+    type: "GET",
+    url: teamsUrl,
+    dataType: 'json',
+    headers: {
+      "Authorization": "Basic " + btoa(api + ":" + "MYSPORTSFEEDS")
+    },
+
+  })
+  .then(function (teamStats) {
+    console.log(teamStats)
+
+
+
+    $.ajax
+      ({
+        type: "GET",
+        url: gameUrl,
+        dataType: 'json',
+        headers: {
+          "Authorization": "Basic " + btoa(api + ":" + "MYSPORTSFEEDS")
+        },
+
+      })
+      .then(function (response) {
+        console.log(response);
+
+
+
+        for (i = 0; i < response.games.length; i++) {
+
+          var dateFromAPI = response.games[i].schedule.startTime;
+          var localDate = new Date(dateFromAPI);
+          var localDateString = localDate.toLocaleDateString(undefined, {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric'
+          })
+
+          var localTimeString = localDate.toLocaleTimeString(undefined, {
+            hour: '2-digit',
+            minute: '2-digit',
+          })
+
+          var hTeam = response.games[i].schedule.homeTeam.abbreviation;
+          var aTeam = response.games[i].schedule.awayTeam.abbreviation;
+          var aScore = response.games[i].score.awayScoreTotal;
+          var hScore = response.games[i].score.homeScoreTotal;
+          var status = response.games[i].schedule.playedStatus;
+          var quarter = response.games[i].score.currentQuarter
+
+          function myTime(time) {
+            var hr = ~~(time / 3600);
+            var min = ~~((time % 3600) / 60);
+            var sec = time % 60;
+            var sec_min = "";
+            if (hr > 0) {
+              sec_min += "" + hrs + ":" + (min < 10 ? "0" : "");
+            }
+            sec_min += "" + min + ":" + (sec < 10 ? "0" : "");
+            sec_min += "" + sec;
+            return sec_min;
+          }
+          var timeRemaining = (myTime(response.games[i].score.currentQuarterSecondsRemaining));
+
+          if (timeRemaining == "0:00") {
+            timeRemaining = ""
+          } else if (timeRemaining == "0:00" && quarter != null) {
+            timeRemaining = "End of"
+          } else {
+            timeRemaining = timeRemaining + " Q"
+          }
+
+          if (quarter == null) {
+            quarter = ""
+          }
+
+          if (status == "COMPLETED_PENDING_REVIEW" || status == "COMPLETED") {
+            status = "FINAL";
+          } else if (status == "UNPLAYED") {
+            status = " ";
+          } else if (status == "LIVE") {
+            status = timeRemaining + quarter;
+          }
+
+
+          if (aScore == null) {
+            aScore = 0;
+            hScore = 0;
+          }
+
+          var aDisplayName = aTeam
+          var hDisplayName = hTeam
+
+          if (aTeam == "BRO") {
+            aDisplayName = "BKN";
+          } ;
+          if (aTeam == "OKL") {
+            aDisplayName == "OKC";
+          };
+
+          if (hTeam == "BRO") {
+            hDisplayName = "BKN";
+          } ;
+          if (hTeam == "OKL") {
+            hDisplayName == "OKC";
+          };
+
+          var htmlString = '<div class="card  text-dark rounded m-1" style="width: 187px;" id='+aTeam + hTeam+'>';
+          htmlString += '<div class="row mr-1 ml-1 ">';
+          htmlString += '<table class="table  table-borderless table-sm  mt-1 mb-1" style="background-color: rgba(245, 245, 245, .7) !important;" >';
+          htmlString += '<thead class="table-borderless">';
+          htmlString += '<tr>';
+          htmlString += '<th class="table-borderless  pt-0" style=" font-size: 11px;">' + localTimeString + '</th>';
+          htmlString += '<th class="table-borderless  text-danger text-center pt-0" style=" font-size: 11px;"><small></small></th>';
+          htmlString += '<th class="table-borderless  text-danger text-right pt-0" style=" font-size: 11px;">' + status + '</th>';
+          htmlString += '</tr>';
+          htmlString += '</thead>';
+          htmlString += '<tbody>';
+          htmlString += '<tr>';
+          htmlString += '<th scope="row" class="table-borderless align-middle text-left  pt-0 pr-0" style="font-size: 12px;" ><img src="images/logos/' + aTeam + '.png" height="25px" width="25px">' + " " + aDisplayName + '</th>'
+          htmlString += '<td class="table-borderless align-middle text-left text-muted  pt-0 pl-0" id="'+aTeam+'record" style=" font-size: 10px;"> </td>';
+          htmlString += '<td class="table-borderless  align-middle pt-0 text-right" style=" font-size: 14px;"><strong>' + aScore + '</strong></td>';
+          htmlString += '</tr>';
+          htmlString += '<tr>';
+          htmlString += '<th scope="row" class="align-middle text-left  pt-0 pr-0" style="font-size: 12px;"><img src="images/logos/' + hTeam + '.png" height="25px" width="25px">' + " " + hDisplayName + '</th>'
+          htmlString += '<td class="align-middle text-left text-muted  pt-0 pl-0" id="'+hTeam+'record" style=" font-size: 10px;"> </td>';
+          htmlString += '<td class="  align-middle  pt-0 text-right" style=" font-size: 14px;"><strong>' + hScore + '</strong></td>';
+          htmlString += '</tr>';  
+          htmlString += '</tbody>';      
+          htmlString += '</table>';
+          htmlString += '</div>';
+          htmlString += '</div>';
+          $("#schedule-holder").append(htmlString);      // Append the new elements 
+
+
+  
+
+          for (j = 0; j < teamStats.teamStatsTotals.length; j++) {
+
+
+
+            if (teamStats.teamStatsTotals[j].team.abbreviation == aTeam) {
+              var aTeamStats = teamStats.teamStatsTotals[j];
+              var aTeamWinPct = teamStats.teamStatsTotals[j].stats.standings.winPct;
+              $("#"+aTeam+"record").text(teamStats.teamStatsTotals[j].stats.standings.wins + "-"+teamStats.teamStatsTotals[j].stats.standings.losses);  
+              // if (teamStats.teamStatsTotals[j].stats.standings.winPct > .5) {
+              //   $("#"+aTeam+hTeam).addClass("border-success")
+              // }   // Append the new elements 
+            }
+            if (teamStats.teamStatsTotals[j].team.abbreviation == hTeam) {
+              var hTeamStats = teamStats.teamStatsTotals[j]
+              var hTeamWinPct = teamStats.teamStatsTotals[j].stats.standings.winPct;
+              $("#"+hTeam+"record").text(teamStats.teamStatsTotals[j].stats.standings.wins + "-"+teamStats.teamStatsTotals[j].stats.standings.losses);      // Append the new elements 
+            }
+          }
+
+          if (aTeamWinPct > .615 && hTeamWinPct > .615) {
+            $("#"+aTeam+hTeam).addClass("rgba-cyan-light");     
+          } else if (aTeamWinPct > .525 && hTeamWinPct > .525 || aTeamWinPct + hTeamWinPct > 1.15) {
+            $("#"+aTeam+hTeam).addClass("rgba-green-light");      
+          }  else if (aTeamWinPct > .425 && hTeamWinPct > .425 || aTeamWinPct + hTeamWinPct > .95) {
+            $("#"+aTeam+hTeam).addClass("rgba-yellow-light");      
+          } else if (aTeamWinPct > .3 && hTeamWinPct > .3 || aTeamWinPct + hTeamWinPct > .7) {
+            $("#"+aTeam+hTeam).addClass(" rgba-orange-light");      
+          }else {
+            $("#"+aTeam+hTeam).addClass("rgba-red-light");      
+          }
+
+        };
+
+
+      });
+  });
+
+}
+
+schedule(today);
+
+$("#yesterday").click(function() {
+  $("#schedule-holder").empty()
+  today.setDate(today.getDate() - 1)
+  schedule(today);
+});
+$("#tomorrow").click(function() {
+  $("#schedule-holder").empty()
+  today.setDate(today.getDate() + 1)
+  schedule(today);
+});
---
+++ ministandings.js
@@ -0,0 +1,39 @@
+var teamsUrl = "https://api.mysportsfeeds.com/v2.0/pull/nba/current/standings.json"
+
+
+var api = config.MY_KEY
+
+
+
+
+$.ajax
+    ({
+        type: "GET",
+        url: teamsUrl,
+        dataType: 'json',
+        headers: {
+            "Authorization": "Basic " + btoa(api + ":" + "MYSPORTSFEEDS")
+        },
+
+    })
+    .then(function (standings) {
+        console.log(standings)
+
+        for (j = 0; j < standings.teams.length; j++) {
+            if (standings.teams[j].playoffRank.conferenceName == "Western") {
+                var miniStandingsStringW = '<td class="align-middle text-left pt-1 pb-1" style="font-size: 10px;">'+ standings.teams[j].playoffRank.rank + '</td>'
+                miniStandingsStringW += '<td class="align-middle text-left pl-0 pt-1 pb-1" style="font-size: 13px;"><img src="images/logos/' + standings.teams[j].team.abbreviation + '.png" height="20px" width="20px">'+ " " +standings.teams[j].team.city + " " + standings.teams[j].team.name + '</td>'
+                miniStandingsStringW += '<td class="align-middle text-center pr-0 pt-1 pb-1" style="font-size: 10px;">' + standings.teams[j].stats.standings.wins + "-" + standings.teams[j].stats.standings.losses +'</td>'
+                miniStandingsStringW += '<td class="align-middle text-right pl-0 pt-1 pb-1" style="font-size: 10px;">' + standings.teams[j].conferenceRank.gamesBack + '</td>'
+                $("#wTeam"+standings.teams[j].playoffRank.rank).append(miniStandingsStringW)
+            } else {
+                var miniStandingsStringE = '<td class="align-middle text-left pt-1 pb-1" style="font-size: 10px;">'+ standings.teams[j].playoffRank.rank + '</td>'
+                miniStandingsStringE += '<td class="align-middle text-left pl-0 pt-1 pb-1" style="font-size: 13px;"><img src="images/logos/' + standings.teams[j].team.abbreviation + '.png" height="20px" width="20px">'+ " " +standings.teams[j].team.city + " " + standings.teams[j].team.name + '</td>'
+                miniStandingsStringE += '<td class="align-middle text-center pr-0 pt-1 pb-1" style="font-size: 10px;">' + standings.teams[j].stats.standings.wins + "-" + standings.teams[j].stats.standings.losses + '</td>'
+                miniStandingsStringE += '<td class="align-middle text-right pl-0 pt-1 pb-1" style="font-size: 10px;">' + standings.teams[j].conferenceRank.gamesBack + '</td>'
+                $("#eTeam"+standings.teams[j].playoffRank.rank).append(miniStandingsStringE)
+
+            }
+
+        };
+    });
