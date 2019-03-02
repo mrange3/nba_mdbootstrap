a5e3be7c8f916dec59b1ed04d4b097fe8ce85d6d
mrange3 <41908065+mrange3@users.noreply.github.com>
2019-03-01 21:59:50 -0500

    made scores responsive

--- javascript.js
+++ javascript.js
@@ -155,9 +155,9 @@ $.ajax
          var aLogo = aTeam.toLowerCase();
          var hLogo = hTeam.toLowerCase();
 
-          var htmlString = '<div class="card  text-dark rounded m-1" style="width: 187px;" id='+aTeam + hTeam+'>';
-          htmlString += '<div class="row mr-1 ml-1 ">';
-          htmlString += '<table class="table  table-borderless table-sm  mt-1 mb-1" style="background-color: rgba(245, 245, 245, .7) !important;" >';
+          var htmlString = '<div class="col-4 p-0">';
+          htmlString += '<div class="card  text-dark  rounded m-2" " id='+aTeam + hTeam+'>';
+          htmlString += '<table class="table  table-borderless table-sm w-90 m-3 p-2" style="background-color: rgba(245, 245, 245, .7) !important;" >';
           htmlString += '<thead class="table-borderless">';
           htmlString += '<tr>';
           htmlString += '<th class="table-borderless  pt-0" style=" font-size: 11px;">' + localTimeString + '</th>';
@@ -167,14 +167,14 @@ $.ajax
           htmlString += '</thead>';
           htmlString += '<tbody>';
           htmlString += '<tr>';
-          htmlString += '<th scope="row" class="table-borderless align-middle text-left  pt-0 pr-0" style="font-size: 12px;" ><img src="images/logos/' + aLogo + '.png" height="25px" width="25px">' + " " + aDisplayName + '</th>'
-          htmlString += '<td class="table-borderless align-middle text-left text-muted  pt-0 pl-0" id="'+aTeam+'record" style=" font-size: 10px;"> </td>';
+          htmlString += '<th scope="row" class="table-borderless align-middle text-left  pt-0 " style="font-size: 12px;" ><img src="images/logos/' + aLogo + '.png" height="25px" width="25px">' + " " + aDisplayName + '</th>'
+          htmlString += '<td class="table-borderless align-middle text-left text-muted  pt-0 " id="'+aTeam+'record" style=" font-size: 10px;"> </td>';
           htmlString += '<td class="table-borderless  align-middle pt-0 text-right" style=" font-size: 14px;"><strong>' + aScore + '</strong></td>';
           htmlString += '</tr>';
           htmlString += '<tr>';
-          htmlString += '<th scope="row" class="align-middle text-left  pt-0 pr-0" style="font-size: 12px;"><img src="images/logos/' + hLogo + '.png" height="25px" width="25px">' + " " + hDisplayName + '</th>'
-          htmlString += '<td class="align-middle text-left text-muted  pt-0 pl-0" id="'+hTeam+'record" style=" font-size: 10px;"> </td>';
-          htmlString += '<td class="  align-middle  pt-0 text-right" style=" font-size: 14px;"><strong>' + hScore + '</strong></td>';
+          htmlString += '<th scope="row" class="align-middle text-left  pt-0 " style="font-size: 12px;"><img src="images/logos/' + hLogo + '.png" height="25px" width="25px">' + " " + hDisplayName + '</th>'
+          htmlString += '<td class="align-middle text-left text-muted  pt-0 " id="'+hTeam+'record" style=" font-size: 10px;"> </td>';
+          htmlString += '<td class="  align-middle  pt-0 text-right " style=" font-size: 14px;"><strong>' + hScore + '</strong></td>';
           htmlString += '</tr>';  
           htmlString += '</tbody>';      
           htmlString += '</table>';
