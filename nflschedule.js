eb5dc3dffd36842072171f7d703ed75d2efc3112
mrange3 <41908065+mrange3@users.noreply.github.com>
2019-10-01 17:34:33 -0400

    fixed tabs

--- nflschedule.js
+++ nflschedule.js
@@ -305,32 +305,32 @@ function nflSchedule(scheduledWeek) {
           htmlString += '</table>';
 
 
-          // htmlString += '<tr class=" row d-flex nav-tabs ">';
+          // htmlString += '<tr class=" row ">';
           // htmlString += '<td class="col  text-left py-1 pl-3 font-weight-bold"  style=" font-size: 16px;"></td>';
-          // htmlString += '<td class="col-1  text-center px-0 py-1 font-weight-bold  " style=" font-size: 14px;"></td>';
+          // htmlString += '<td class="col-1  text-center px-0 py-1 font-weight-bold nav-link " id="Q1'+i+'" data-toggle="tab" href="#scoringSummary'+gameID+ '" role="tab" aria-controls="scoringSummary' + gameID + '" style=" font-size: 14px;">Q1</td>';
           // htmlString += '<td class="col-1  text-center px-0 py-1 font-weight-bold  " style=" font-size: 14px;">Q2</td>';
           // htmlString += '<td class="col-1  text-center px-0 py-1 font-weight-bold  " style=" font-size: 14px;">Q3</td>';
           // htmlString += '<td class="col-1  text-center px-0 py-1 font-weight-bold  " style=" font-size: 14px;">Q4</td>';
           // htmlString += '<td class="col text-center font-weight-bold  py-1 pr-3"  style=" font-size: 16px;"></td>';
           // htmlString += '</tr>';
-          
           htmlString += '<div class="row justify-content-center py-0 px-0 my-0 ">'
-
-          htmlString += '<div class="d-flex justify-content-center p-0 my-0 ">'
-          htmlString += '<ul class="nav nav-tabs py-0 d-flex justify-content-around" id="quartersScoring" role="tablist">'
-          htmlString += '<li class="nav-item  ">'
+          
+          htmlString += '<div class="d-flex justify-content-center py-0 px-0 my-0 ">'
+          htmlString += '<ul class="nav  nav-tabs py-0 " id="quartersScoring" role="tablist">'
+          htmlString += '<li class="nav-item m-0 p-0 ">'
           htmlString += '<a class="nav-link active py-0 px-1 m-0 quarter-1 text-dark" id="Q1-tab' + gameID + '" data-toggle="tab" href="#Q1'+gameID+ '" role="tab" aria-controls="Q1' + gameID + '" style=" font-size: 14px;" aria-selected="true">Q1</a>'
           htmlString += '</li>'
-          htmlString += '<li class="nav-item ">'
+          htmlString += '<li class="nav-item m-0 p-0">'
           htmlString += '<a class="nav-link py-0 px-1 m-0 quarter text-dark" id="Q2-tab' + gameID + '" data-toggle="tab" href="#Q2'+gameID+ '" role="tab" aria-controls="Q2' + gameID + '" style=" font-size: 14px;" aria-selected="false">Q2</a>'
           htmlString += '</li>'
-          htmlString += '<li class="nav-item ">'
+          htmlString += '<li class="nav-item  m-0 p-0">'
           htmlString += '<a class="nav-link py-0 px-1 m-0 quarter text-dark" id="Q3-tab' + gameID + '" data-toggle="tab" href="#Q3'+gameID+ '" role="tab" aria-controls="Q3' + gameID + '" style=" font-size: 14px;" aria-selected="false">Q3</a>'
           htmlString += '</li>'
-          htmlString += '<li class="nav-item ">'
+          htmlString += '<li class="nav-item m-0 p-0">'
           htmlString += '<a class="nav-link py-0 px-1 m-0 quarter text-dark" id="Q4-tab' + gameID + '" data-toggle="tab" href="#Q4'+gameID+ '" role="tab" aria-controls="Q4' + gameID + '" style=" font-size: 14px;" aria-selected="false">Q4</a>'
           htmlString += '</li>'
           htmlString += '</ul>'
+          htmlString += '</div>'
           htmlString += '</div>'
 
           htmlString += '<div class="tab-content ">'
@@ -341,8 +341,6 @@ function nflSchedule(scheduledWeek) {
           htmlString += '<div class="tab-pane fade show  text-center" id="Q4' + gameID + '" role="tabpanel" aria-labelledby="Q4-tab' + gameID+ '"></div>'
 
           htmlString += '</div>';
-
-
           htmlString += '</div>';
 
 
