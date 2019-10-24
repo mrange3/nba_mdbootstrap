f00089852b280bbf00eb9c5535a2d534fbef5b69
mrange3 <41908065+mrange3@users.noreply.github.com>
2019-10-24 12:34:40 -0400

    updated margins

--- javascript.js
+++ javascript.js
@@ -124,18 +124,21 @@ $.ajax
           }else if (timeRemaining == "0:00" && quarter > 0) {
             timeRemaining = "End of"
           }
-          else {
+          else if (quarter > 0 && quarter < 4){
             timeRemaining = timeRemaining + " Q" + quarter;
+          } else if (quarter > 4) {
+            timeRemaining = timeRemaining + " OT" + quarter -4;
+
           }
 
           if (intermission == 2) {
             timeRemaining = "Halftime"
-          } else if (intermission == 3) {
-            timeRemaining = "End of Q3"
-          } else if (intermission == 1) {
-            timeRemaining = "End of Q1"
+          } else if (intermission > 0 && intermission < 4) {
+            timeRemaining = "End of Q" + intermission
+          } else if (intermission >4) {
+            timeRemaining = "End of OT" + intermission -4
           }
-
+          
 
 
           if (status == "COMPLETED_PENDING_REVIEW" || status == "COMPLETED") {
@@ -155,27 +158,27 @@ $.ajax
          var aLogo = aTeam.toLowerCase();
          var hLogo = hTeam.toLowerCase();
 
-          var htmlString = '<div class="col-4 col-lg-3 scoreCard px-1 py-1">';
+          var htmlString = '<div class="col-4 col-lg-3 scoreCard px-lg-1 py-lg-1 px-0 py-0">';
           htmlString += '<div class="card justify-content-center rounded-0 text-dark m-1" id='+aTeam + hTeam+'>';
-          htmlString += '<div class="m-2">'
+          htmlString += '<div class="m-1 m-lg-2">'
           htmlString += '<table class="table table-borderless    table-sm w-85 m-0" id='+aTeam + hTeam+"table"+'  >';
           htmlString += '<thead class="table-borderless">';
           htmlString += '<tr>';
-          htmlString += '<th class="table-borderless scoreboard-header p-0" id='+aTeam + hTeam+"clock"+' style=" font-size: 11px;">' + scoreStatus + '</th>';
+          htmlString += '<th class="table-borderless scoreboard-header py-0 pr-0 pl-1" id='+aTeam + hTeam+"clock"+' style=" font-size: 11px;">' + scoreStatus + '</th>';
           htmlString += '<th class="table-borderless  scoreboard-header p-0" ></th>';
           htmlString += '<th class="table-borderless scoreboard-header text-right text-dark p-0" style=" font-size: 11px;" id='+'gs'+aTeam + hTeam+'></th>';
           htmlString += '</tr>';
           htmlString += '</thead>';
           htmlString += '<tbody>';
           htmlString += '<tr>';
-          htmlString += '<th scope="row" class="table-borderless scoreboard align-middle text-left p-0" id='+aTeamID+"name"+' style="font-size: 13px;" ><img class="scoreLogo" src="images/logos/' + aLogo + '.png" height="24px" width="24px"></th>'
+          htmlString += '<th scope="row" class="table-borderless scoreboard align-middle text-left py-0 pr-0 pl-1" id='+aTeamID+"name"+' style="font-size: 13px;" ><img class="scoreLogo" src="images/logos/' + aLogo + '.png" height="24px" width="24px"></th>'
           htmlString += '<td hidden class="table-borderless p-0 align-middle teamRecord text-center" id="'+aTeam+'record" style=" font-size: 12px;"></td>';
-          htmlString += '<td class="table-borderless text-right scoreboard align-middle p-0" id="'+aTeam+'score"  style=" font-size: 13px;"><strong>' + aScore + '</strong></td>';
+          htmlString += '<td class="table-borderless text-right scoreboard align-middle py-0 pr-1 pl-0" id="'+aTeam+'score"  style=" font-size: 13px;"><strong>' + aScore + '</strong></td>';
           htmlString += '</tr>';
           htmlString += '<tr>';
-          htmlString += '<th scope="row" class="align-middle text-left scoreboard py-0 px-0" id='+hTeamID+"name"+' style="font-size: 13px;"><img class="scoreLogo" src="images/logos/' + hLogo + '.png" height="24px" width="24px"></th>'
+          htmlString += '<th scope="row" class="align-middle text-left scoreboard py-0 pr-0 pl-1" id='+hTeamID+"name"+' style="font-size: 13px;"><img class="scoreLogo" src="images/logos/' + hLogo + '.png" height="24px" width="24px"></th>'
           htmlString += '<td hidden class="align-middle text-center p-0 teamRecord " id="'+hTeam+'record" style=" font-size: 12px;"></td>';
-          htmlString += '<td class="  align-middle scoreboard  text-right  p-0" id="'+hTeam+'score" style=" font-size: 13px;"><strong>' + hScore + '</strong></td>';
+          htmlString += '<td class="  align-middle scoreboard  text-right  py-0 pr-1" id="'+hTeam+'score" style=" font-size: 13px;"><strong>' + hScore + '</strong></td>';
           htmlString += '</tr>';  
           htmlString += '</tbody>';      
           htmlString += '</table>';
