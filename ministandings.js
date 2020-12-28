cc9d5acee3a6d9ce850f079207849af8fca8afda
mrange3 <41908065+mrange3@users.noreply.github.com>
2020-12-28 13:25:30 -0500

    New API

--- ministandings.js
+++ ministandings.js
@@ -1,89 +1,88 @@
-var teamsUrl = "https://api.mysportsfeeds.com/v2.0/pull/nba/current/standings.json"
-
-
-var api = config.MY_KEY
-
-
+var teamsUrl = "http://data.nba.net/10s/prod/v1/current/standings_conference.json"
 
 $.ajax
     ({
         type: "GET",
         url: teamsUrl,
         dataType: 'json',
-        headers: {
-            "Authorization": "Basic " + btoa(api + ":" + "MYSPORTSFEEDS")
-        },
 
     })
     .then(function (standings) {
         console.log(standings)
 
         // /Main Standings///////////////
-        for (j = 0; j < standings.teams.length; j++) {
-            tLogo = standings.teams[j].team.abbreviation.toLowerCase();
-            var diff = Math.round(10*(standings.teams[j].stats.offense.ptsPerGame - standings.teams[j].stats.defense.ptsAgainstPerGame))/10;
-            var tRebounds = Math.round(10*(standings.teams[j].stats.rebounds.defRebPerGame + standings.teams[j].stats.rebounds.offRebPerGame))/10;
-            var playoffSeed = standings.teams[j].playoffRank.rank;
-            
-            if (playoffSeed > 8) {
-                playoffSeed = "";
+        for (j = 0; j < standings.league.standard.conference.west.length; j++) {
+            wtLogo = standings.league.standard.conference.west[j].teamSitesOnly.teamTricode.toLowerCase();
+            var wplayoffSeed = standings.league.standard.conference.west[j].confRank;
+            var wteamStandingsAbv = standings.league.standard.conference.west[j].teamSitesOnly.teamTricode;
+            var wteamWins = standings.league.standard.conference.west[j].win;
+            var wteamLoss = standings.league.standard.conference.west[j].loss;
+
+            if (wplayoffSeed > 8) {
+                wplayoffSeed = "";
             }
 
 
-            if (diff > 0) {
-                diff = "+" + diff
-            }
-
-            var teamStandingsAbv = standings.teams[j].team.abbreviation;
-
-            if (teamStandingsAbv == "OKL") {
-                teamStandingsAbv = "OKC"
-              }
-              if (teamStandingsAbv == "BRO") {
-                teamStandingsAbv = "BKN"
-              }
-              $(".clickShow").hide();
-              $(".clickShow2").hide();
 
 
-            if (standings.teams[j].playoffRank.conferenceName == "Western") {
-                var standingsStringW = '<td class="dateFont mainDate align-middle text-center py-0 px-1 m-0 font-weight-bold" style="font-size: 12px; ">'+ playoffSeed + '</td>'
-                standingsStringW += '<th class="dateFont mainDate align-middle clickHide text-left py-0 pl-0 pr-1 text-nowrap boxtable " style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + tLogo + '.png" >'+ " " +teamStandingsAbv+ '</th>'
-                standingsStringW += '<th  class="dateFont   align-middle text-left py-0 pl-0 pr-1 text-nowrap boxtable dateFont westernStats collapse" style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + tLogo + '.png" >'+ " " +standings.teams[j].team.city+ " " + standings.teams[j].team.name+'</th>'
-                standingsStringW += '<th class="dateFont mainDate align-middle text-center py-0 px-1 teamstatsData dateFont " style="font-size: 16px;">' + standings.teams[j].stats.standings.wins + "-" + standings.teams[j].stats.standings.losses +'</th>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1  teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + " " + standings.teams[j].conferenceRank.gamesBack + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.offense.ptsPerGame + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.ptsAgainstPerGame + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.offense.astPerGame + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.blkPerGame + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.stlPerGame + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + tRebounds + '</td>'
-                standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 plusMinus teamstatsData collapse westernStats"   aria-labelledby="headingOne" data-parent="#westernConference">' + diff + '</td>'
+            //   $(".clickShow").hide();
+            //   $(".clickShow2").hide();
 
-                $("#wSTeam"+standings.teams[j].playoffRank.rank).append(standingsStringW)
-            } else {
-                var standingsStringE = '<td class="dateFont mainDate align-middle text-center py-0 px-1 m-0 font-weight-bold" style="font-size: 12px;">'+ playoffSeed + '</td>'
-                standingsStringE += '<th class="dateFont mainDate align-middle clickHide2 text-left py-0  pl-0 pr-1 text-nowrap boxteam boxtable " style="font-size: 16px;"><img class="p-0 m-0 smallLogo" src="images/logos/' + tLogo + '.png" >'+ " " +teamStandingsAbv  + '</th>'
-                standingsStringE += '<th  class="dateFont mainDate collapse easternStats align-middle text-left py-0 pl-0 pr-4 text-nowrap boxtable " style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + tLogo + '.png" >'+ " " +standings.teams[j].team.city+ " " + standings.teams[j].team.name+'</th>'
-                standingsStringE += '<th class="dateFont mainDate align-middle text-center py-0 px-1 teamstatsData " style="font-size: 16px;">' + standings.teams[j].stats.standings.wins + "-" + standings.teams[j].stats.standings.losses +'</th>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + standings.teams[j].conferenceRank.gamesBack +'</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + standings.teams[j].stats.offense.ptsPerGame + '</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + standings.teams[j].stats.defense.ptsAgainstPerGame + '</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + standings.teams[j].stats.offense.astPerGame + '</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + standings.teams[j].stats.defense.blkPerGame + '</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + standings.teams[j].stats.defense.stlPerGame + '</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + tRebounds + '</td>'
-                standingsStringE += '<td class="dateFont mainDate align-middle text-center py-0 px-1 plusMinusteamstatsData collapse easternStats"  aria-labelledby="headingOne" data-parent="#easternConference">' + diff + '</td>'
-                $("#eSTeam"+standings.teams[j].playoffRank.rank).append(standingsStringE)
 
-            }
+                var standingsStringW = '<td class="dateFont mainDate align-middle text-center py-0 px-1 m-0 font-weight-bold" style="font-size: 12px; ">'+ wplayoffSeed + '</td>'
+                standingsStringW += '<th class="dateFont mainDate align-middle clickHide text-left py-0 pl-0 pr-1 text-nowrap boxtable " style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + wtLogo + '.png" >'+ " " +wteamStandingsAbv+ '</th>'
+                // standingsStringW += '<th  class="dateFont   align-middle text-left py-0 pl-0 pr-1 text-nowrap boxtable dateFont westernStats collapse" style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + tLogo + '.png" >'+ " " +standings.teams[j].team.city+ " " + standings.teams[j].team.name+'</th>'
+                standingsStringW += '<th class="dateFont mainDate align-middle text-center py-0 px-1 teamstatsData dateFont " style="font-size: 16px;">' + wteamWins + "-" + wteamLoss +'</th>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1  teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + " " + standings.teams[j].conferenceRank.gamesBack + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.offense.ptsPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.ptsAgainstPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.offense.astPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.blkPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.stlPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + tRebounds + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 plusMinus teamstatsData collapse westernStats"   aria-labelledby="headingOne" data-parent="#westernConference">' + diff + '</td>'
 
-            // if (Math.round(10*(standings.teams[j].stats.offense.ptsPerGame - standings.teams[j].stats.defense.ptsAgainstPerGame))/10 < 0) {
-            //     $("#"+tLogo+"pointDiff").addClass("text-danger")
-            // } else {
-            //     $("#"+tLogo+"pointDiff").addClass("text-success")
+                $("#wSTeam"+[j+1]).append(standingsStringW)
 
-            // }
+
+                
         };
+        for (j = 0; j < standings.league.standard.conference.east.length; j++) {
+            etLogo = standings.league.standard.conference.east[j].teamSitesOnly.teamTricode.toLowerCase();
+            var eplayoffSeed = standings.league.standard.conference.east[j].confRank;
+            var eteamStandingsAbv = standings.league.standard.conference.east[j].teamSitesOnly.teamTricode;
+            var eteamWins = standings.league.standard.conference.east[j].win;
+            var eteamLoss = standings.league.standard.conference.east[j].loss;
+
+            if (eplayoffSeed > 8) {
+                eplayoffSeed = "";
+            }
+
+
+
+
+            //   $(".clickShow").hide();
+            //   $(".clickShow2").hide();
+
+
+                var standingsStringE = '<td class="dateFont mainDate align-middle text-center py-0 px-1 m-0 font-weight-bold" style="font-size: 12px; ">'+ eplayoffSeed + '</td>'
+                standingsStringE += '<th class="dateFont mainDate align-middle clickHide text-left py-0 pl-0 pr-1 text-nowrap boxtable " style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + etLogo + '.png" >'+ " " +eteamStandingsAbv+ '</th>'
+                // standingsStringW += '<th  class="dateFont   align-middle text-left py-0 pl-0 pr-1 text-nowrap boxtable dateFont westernStats collapse" style="font-size: 16px;"><img class=" smallLogo" src="images/logos/' + tLogo + '.png" >'+ " " +standings.teams[j].team.city+ " " + standings.teams[j].team.name+'</th>'
+                standingsStringE += '<th class="dateFont mainDate align-middle text-center py-0 px-1 teamstatsData dateFont " style="font-size: 16px;">' + eteamWins + "-" + eteamLoss +'</th>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1  teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + " " + standings.teams[j].conferenceRank.gamesBack + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.offense.ptsPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.ptsAgainstPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.offense.astPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.blkPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + standings.teams[j].stats.defense.stlPerGame + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 mobileHide teamstatsData collapse westernStats"  aria-labelledby="headingOne" data-parent="#westernConference">' + tRebounds + '</td>'
+                // standingsStringW += '<td class="dateFont mainDate align-middle text-center py-0 px-1 plusMinus teamstatsData collapse westernStats"   aria-labelledby="headingOne" data-parent="#westernConference">' + diff + '</td>'
+
+                $("#eSTeam"+[j+1]).append(standingsStringE)
+
+
+                
+        };
+
     });  
     
